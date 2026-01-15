605ab3c2f6b9ed0f0951ef1e40d9930ff14e1af8
Felipe Nascimento de Souza <fn23886@gmail.com>
2026-01-15 10:56:02 -0300

    Adiciona funcionalidade de classificação de emails com retorno de resultado

--- email-ai-classifier/app/main.py
+++ email-ai-classifier/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -11,4 +11,18 @@ def home(request: Request):
     return templates.TemplateResponse(
         "index.html",
         {"request": request}
-        )
+    )
+
+@app.post("/process", response_class=HTMLResponse)
+def process_email(request: Request, email_text: str = Form(...)):
+    # CLASSIFICAÇÃO TEMPORÁRIA (fake)
+    category = "Produtivo"
+
+    return templates.TemplateResponse(
+        "result.html",
+        {
+            "request": request,
+            "email_text": email_text,
+            "category": category
+        }
+    )
